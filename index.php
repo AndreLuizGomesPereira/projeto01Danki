6125919a36038dc5afb962f2d3dad1d9d680a6ae
André Luiz <andregomesenator@gmail.com>
2020-07-07 16:47:39 -0300

    Adicionado seção de especialidades.

--- index.php
+++ index.php
@@ -10,39 +10,61 @@
 
 <body>
     <header>
-        <div class="logo">Logomarca</div>
-        <nav class="desktop">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-        <nav class="mobile">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
+        <div class="center">
+            <div class="logo">Logomarca</div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="center">
     </header>
     <section class="banner-principal">
+
         <form>
             <h2>Qual seu melhor e-mail?</h2>
             <input type="email" name="email" required>
             <input type="submit" name="acao" value="Cadastrar!" required>
         </form>
+        </div>
     </section>
 
     <section class="descricao-autor">
-        <div class="w50">
-            <h2>André Luiz Gomes Pereira</h2>
-            <p>Texto para a Biografia</p>
+        <div class="center">
+            <div class="w50">
+                <h2>André Luiz Gomes Pereira</h2>
+                <p>Texto para a Biografia</p>
+            </div>
+            <div class="w50">
+                <img src="img/perfil.jpg" alt="André Luiz">
+            </div>
         </div>
-        <div class="w50">
-            <img src="img/perfil.jpg" alt="André Luiz">
+    </section>
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+            <div class="box-especialidade">
+                <h3>Icone 1</h3>
+                <h3>CSS3</h3>
+                <p>Texto para a especialidade</p>
+                <h3>Icone 2</h3>
+                <h3>HTML</h3>
+                <p>Texto para a especialidade</p>
+                <h3>Icone 2</h3>
+                <h3>JavaScript</h3>
+                <p>Texto para a especialidade</p>
+            </div>
         </div>
     </section>
 
