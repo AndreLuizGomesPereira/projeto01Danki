51c5787227b8ceea0252b4067717ca45111aded9
André Luiz <andregomesenator@gmail.com>
2020-07-09 21:44:33 -0300

    CSS responsivo no HEADER.

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 <body>
     <header>
         <div class="center">
-            <div class="logo">Logomarca</div>
-            <nav class="desktop">
+            <div class="logo left">Logomarca</div>
+            <nav class="desktop right">
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Sobre</a></li>
@@ -32,44 +32,49 @@
                     <li><a href="#">Contato</a></li>
                 </ul>
             </nav>
-            <div class="center">
+        </div>
     </header>
     <section class="banner-principal">
+        <div class="center">
+            <form>
 
-        <form>
-            <h2>Qual seu melhor e-mail?</h2>
-            <input type="email" name="email" required>
-            <input type="submit" name="acao" value="Cadastrar!" required>
-        </form>
+                <h2>Qual seu melhor e-mail?</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar!" required>
+            </form>
         </div>
     </section>
-
     <section class="descricao-autor">
         <div class="center">
             <div class="w50 left">
                 <h2>André Luiz Gomes Pereira</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
-            <div class="w50 left">
-                <img src="img/perfil.jpg" alt="André Luiz">
-            </div>
-            <div class="clear"></div>
         </div>
+        <div class="w50 left">
+            <img src="img/perfil.jpg" alt="André Luiz">
+        </div>
+        <div class="clear"></div>
     </section>
     <section class="especialidades">
         <div class="center">
             <h2 class="title">Especialidades</h2>
-            <div class="box-especialidade">
+            <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-css3-alt"></i></h3>
                 <h3>CSS3</h3>
                 <p>Texto para a especialidade</p>
+            </div>
+            <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-html5"></i></h3>
                 <h3>HTML5</h3>
                 <p>Texto para a especialidade</p>
+            </div>
+            <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-js-square"></i></h3>
                 <h3>JavaScript</h3>
                 <p>Texto para a especialidade</p>
             </div>
+
         </div>
     </section>
     <section class="extras">
