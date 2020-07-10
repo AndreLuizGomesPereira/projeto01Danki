c9692b0731a2ef2e5c0290363641c17a8e0e3cfc
André Luiz <andregomesenator@gmail.com>
2020-07-09 22:11:33 -0300

    Estilização do Header.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="css/all.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans: 400, 700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300, 400, 700">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Descrição de sistema PHP">
     <title>Projeto - PHP</title>
@@ -15,7 +15,7 @@
 <body>
     <header>
         <div class="center">
-            <div class="logo left">Logomarca</div>
+            <div class="logo left">AleSYS</div>
             <nav class="desktop right">
                 <ul>
                     <li><a href="#">Home</a></li>
@@ -24,7 +24,7 @@
                     <li><a href="#">Contato</a></li>
                 </ul>
             </nav>
-            <nav class="mobile">
+            <nav class="mobile right">
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Sobre</a></li>
@@ -32,6 +32,7 @@
                     <li><a href="#">Contato</a></li>
                 </ul>
             </nav>
+            <div class="clear"></div>
         </div>
     </header>
     <section class="banner-principal">
