628d9bf1953a02afab43c2b007e5e25008fad99d
André Luiz <andregomesenator@gmail.com>
2020-07-13 21:33:41 -0300

    Estilização da seção Especialidades.

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/all.css">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="estilo/all.css">
+    <link rel="stylesheet" href="estilo/fontawesome.min.css">
+    <link rel="stylesheet" href="estilo/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300, 400, 700">
     <link rel="shortcut icon" href="img/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -61,23 +61,23 @@
     </section>
     <section class="especialidades">
         <div class="center">
-            <h2 class="title">Especialidades</h2>
+            <h2 class="title">Minhas especialidades</h2>
             <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-css3-alt"></i></h3>
-                <h3>CSS3</h3>
-                <p>Texto para a especialidade</p>
+                <h4>CSS3</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
             <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-html5"></i></h3>
-                <h3>HTML5</h3>
-                <p>Texto para a especialidade</p>
+                <h4>HTML5</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
             <div class="w33 left box-especialidade">
                 <h3><i class="fab fa-js-square"></i></h3>
-                <h3>JavaScript</h3>
-                <p>Texto para a especialidade</p>
+                <h4>JavaScript</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
-
+            <div class="clear"></div>
         </div>
     </section>
     <section class="extras">
