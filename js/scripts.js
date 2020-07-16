6392f19a99cd27e7b061ce68250366693e4b22ed
André Luiz <andregomesenator@gmail.com>
2020-07-16 19:18:37 -0300

    Iniciado URL amigavel.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ $(function () {
         
 
         if (listaMenu.is(':hidden') == true) {
-            var icone = $('botao-menu-mobile').find('i');
+            var icone = $('.botao-menu-mobile').find('i');
             icone.removeClass('fa-bars');
             icone.addClass('fa-times');
             listaMenu.slideToggle();
         } else {
-            var icone = $('botao-menu-mobile').find('i');
+            var icone = $('.botao-menu-mobile').find('i');
             icone.removeClass('fa-times');
             icone.addClass('fa-bars');
             listaMenu.slideToggle();
