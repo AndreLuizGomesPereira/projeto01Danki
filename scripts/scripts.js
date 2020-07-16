3d272be25eb4de51166018c2baf7a7d315bb6013
André Luiz <andregomesenator@gmail.com>
2020-07-16 18:16:18 -0300

    Ajuste no menu mobile.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,18 +3,19 @@ $(function () {
     $('nav.mobile').click(function () {
         var listaMenu = $('nav.mobile ul');
         
+
         if (listaMenu.is(':hidden') == true) {
-            var icone = $('botao-menu-mobile i');
-            icone.removeClass('fas fa-bars');
-            icone.addClass('fas fa-times');
+            var icone = $('botao-menu-mobile').find('i');
+            icone.removeClass('fa-bars');
+            icone.addClass('fa-times');
             listaMenu.slideToggle();
         } else {
-
-            var icone = $('botao-menu-mobile i');
-            icone.removeClass('fas fa-times');
-            icone.addClass('fas fa-bars');
+            var icone = $('botao-menu-mobile').find('i');
+            icone.removeClass('fa-times');
+            icone.addClass('fa-bars');
             listaMenu.slideToggle();
         }
+
 
     })
 })
