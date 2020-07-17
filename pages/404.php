4afefd337d30b52f8a32f378a954954bbdf8676d
André Luiz <andregomesenator@gmail.com>
2020-07-16 22:40:55 -0300

    Conclusão da Página não encontrada!

--- pages/404.php
+++ pages/404.php
@@ -1,5 +1,6 @@
-<section class="erro-404">
+<section style="padding: 30px 0; margin-bottom: 410px;" class="erro-404">
     <div class="center">
-        <h1><i class="fas fa-exclamation-triangle"></i> Página não encontrada!</h1>
+        <h1 style="text-align: center; color: #444; font-weight: 400;"><i style="color: red; padding-right: 10px;" class="fas fa-exclamation-circle"></i> Página não encontrada!</h1>
     </div>
 </section>
+
