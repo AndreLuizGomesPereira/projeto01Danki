50492692672c8f51b683414317162ac633b81eb2
André Luiz <andregomesenator@gmail.com>
2020-07-20 18:59:34 -0300

    Correção da página de construção.

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,6 @@
-<section style="padding: 30px 0; margin-bottom: 410px;" class="erro-404">
+<section style="padding: 30px 0; margin-bottom: 365px;" class="erro-404">
     <div class="center">
-        <h1 style="text-align: center; color: #444; font-weight: 400;"><i style="color: red; padding-right: 10px;" class="fas fa-exclamation-circle"></i> Página não encontrada!</h1>
+        <h1 style="text-align: center; color: #444; font-weight: 400;"><i style="color: red; padding-right: 10px;" class="fas fa-tools"></i>Página em contrução</h1>
+        <p style="text-align: center; color: #444; margin-top: 20px; font-weight: 400;">Deseja Voltar para a <a href="<?php echo INCLUDE_PATH; ?>">página inicial?</a></p>
     </div>
 </section>
-
