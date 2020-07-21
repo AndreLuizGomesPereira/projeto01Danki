80ade0a9f897b5b1c6f5decb62484fd1236bb7e9
André Luiz <andregomesenator@gmail.com>
2020-07-20 23:02:41 -0300

    Ajuste no layout de contato.

--- pages/contato.php
+++ pages/contato.php
@@ -5,8 +5,11 @@
     <div class="center">
         <form>
             <input type="text" name="nome" placeholder="Nome">
-            <input type="email" name="email" placeholder="E-mail">
-            <input type="number" name="celular" placeholder="Telefone/Celular">
+            <div></div>
+            <input type="text" name="email" placeholder="E-mail">
+            <div></div>
+            <input type="text" name="celular" placeholder="Telefone/Celular">
+            <div></div>
             <textarea name="mensagem"></textarea>
             <input type="submit" name="acao" value="Salvar Dados">
         </form>
